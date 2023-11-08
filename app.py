e5ae654e07d573fc7f6a4a9a397d83f692990873
AlexandruGabriel02 <70471765+AlexandruGabriel02@users.noreply.github.com>
2023-11-08 18:29:57 +0200

    1 and 2 done

--- app.py
+++ app.py
@@ -6,11 +6,29 @@ class Player:
     NO_PLAYER = 'X'
 
 class GameState:
+    magic_square = np.array([[2,7,6],[9,5,1],[4,3,8]])
     def __init__(self):
         self.board = np.full((3, 3), None)
-        self.magic_square = np.array([[2,7,6],[9,5,1],[4,3,8]])
         self.player_to_move = Player.PLAYER_1
 
+    def get_next_player(player):
+        if player == Player.PLAYER_1:
+            return Player.PLAYER_2
+        elif player == Player.PLAYER_2:
+            return Player.PLAYER_1
+        return Player.NO_PLAYER
+
+    def get_next_state(self, move):
+        next_state = GameState()
+        next_state.player_to_move = GameState.get_next_player(self.player_to_move)
+        next_state.board = np.copy(self.board)
+        
+        for i in range(3):
+            for j in range(3):
+                if GameState.magic_square[i][j] == move:
+                    next_state.board[i][j] = self.player_to_move
+                    
+        return next_state
 
 class Validator:
     def is_final_state(state):
@@ -24,5 +42,11 @@ class Validator:
         
         if all(state.board[i][i] == Player.PLAYER_1 or state.board[i][i] == Player.PLAYER_2 for i in range(3)):
             return True, state.board[0][0]
+        if all(state.board[i][2-i] == Player.PLAYER_1 or state.board[i][2-i] == Player.PLAYER_2 for i in range(3)):
+            return True, state.board[0][2]
+        
+        return True, Player.NO_PLAYER
+    
+    
         
             
